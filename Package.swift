c4b93da08525b35baec50342aa32234652951e61
neel mistry <neel.mistry@mapbox.com>
2021-01-15 12:46:41 -0500

    julian

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["MapboxCoreMapsWrapper"]),
     ],
     dependencies: [
-        .package(name: "MapboxCommon", url: "git@github.com:mapbox/mapbox-common-ios.git", .exact("10.0.0-beta.4")),
+        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", .exact("10.0.0-beta.4")),
     ],
     targets: [
         .target(
