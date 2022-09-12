24074825cfac3a71fb7a6a8749ddf848383e020b
Roman Laitarenko <roman.laitarenko@mapbox.com>
2022-09-12 13:29:09 +0300

    Specs for 10.7.0-73ce7a5019-SNAPSHOT

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let version = "10.8.0"
-let checksum = "fa2e3aa92e864a4a86543f5cf086fc1dce5fcbb54ce413a73c38dbe5662112ba"
+let version = "10.7.0-73ce7a5019-SNAPSHOT"
+let checksum = "b6fe94f10feae961f0367239b7367128aa2ce6aefdd1d63f2ccc9590d56765fc"
 
 let package = Package(
     name: "MapboxCoreMaps",
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["MapboxCoreMapsWrapper"]),
     ],
     dependencies: [
-        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", from: "23.0.0"),
+        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", from: "22.1.0"),
     ],
     targets: [
         .target(
@@ -25,7 +25,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "MapboxCoreMaps",
-            url: "https://api.mapbox.com/downloads/v2/mobile-maps-core/releases/ios/packages/\(version)/MapboxCoreMaps.xcframework-dynamic.zip",
+            url: "https://api.mapbox.com/downloads/v2/mobile-maps-core/snapshots/ios/packages/\(version)/MapboxCoreMaps.xcframework-dynamic.zip",
             checksum: checksum
         )
     ]
