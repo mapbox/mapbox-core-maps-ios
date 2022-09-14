e101dbc61bdf828beb7bcf477ba7073e04fd7999
Roman Laitarenko <roman.laitarenko@mapbox.com>
2022-09-14 14:04:38 +0300

    Specs for 10.8.0-6110902782-SNAPSHOT

--- MapboxCoreMaps.podspec
+++ MapboxCoreMaps.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |m|
 
-  version = '10.7.0-73ce7a5019-SNAPSHOT'
+  version = '10.8.0-6110902782-SNAPSHOT'
 
   m.name = 'MapboxCoreMaps'
   m.version = version
@@ -22,6 +22,6 @@ Pod::Spec.new do |m|
 
   m.vendored_frameworks = 'MapboxCoreMaps.xcframework'
 
-  m.dependency 'MapboxCommon', '~> 21.1.0'
+  m.dependency 'MapboxCommon', '~> 23.0'
 
 end
--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let version = "10.7.0-73ce7a5019-SNAPSHOT"
-let checksum = "b6fe94f10feae961f0367239b7367128aa2ce6aefdd1d63f2ccc9590d56765fc"
+let version = "10.8.0-6110902782-SNAPSHOT"
+let checksum = "177f5149caf8b8bcf87ee6d58089c42a644abf1ce3618eae1ad4a2cc2b83b638"
 
 let package = Package(
     name: "MapboxCoreMaps",
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["MapboxCoreMapsWrapper"]),
     ],
     dependencies: [
-        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", from: "22.1.0"),
+        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", from: "23.0.0"),
     ],
     targets: [
         .target(
