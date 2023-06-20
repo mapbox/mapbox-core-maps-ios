728388e2d6db1edf2653a814cb2dfe3f8ee551c3
Release SDK bot for GL-Native Core team <gl-native-team@mapbox.com>
2023-06-20 08:28:56 +0000

    Update SPM and CocoaPods configs for 10.12.3 release

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let version = "10.14.0"
-let checksum = "ac7c21560b8e9666365d00526bbf50de998f7fdcd091bcf106d03dcde1770b70"
+let version = "10.12.3"
+let checksum = "1098b95ed42a63e3130e5da83023a604ab26d2818af46a5b4d891b654c8a5c2c"
 
 let package = Package(
     name: "MapboxCoreMaps",
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["MapboxCoreMapsWrapper"]),
     ],
     dependencies: [
-        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", from: "23.6.0"),
+        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", from: "23.4.0"),
     ],
     targets: [
         .target(
