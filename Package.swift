f12f53d699cf5b1ac14673a2fe32da7e36994afe
Ivan Persidsky <ivan.persidskii@mapbox.com>
2024-01-30 23:36:05 +0200

    Add visionOS version in SPM

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,7 @@ let checksum = "e57f8408728246dd1ca56c3b6affca5b33c81424a741d42bb7a193de890a0873
 
 let package = Package(
     name: "MapboxCoreMaps",
-    platforms: [.iOS(.v10), .macOS(.v10_15)],
+    platforms: [.iOS(.v10), .macOS(.v10_15), .custom("visionos", versionString: "1.0")],
     products: [
         .library(
             name: "MapboxCoreMaps",
