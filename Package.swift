9d4dd08cb10e3d9cb51e932717b3507f70e924f2
Roman Laitarenko <roman.laitarenko@mapbox.com>
2024-08-29 13:01:34 +0300

    Address warnings in Package.swift

--- Package.swift
+++ Package.swift
@@ -9,19 +9,19 @@ let checksum = "834e87df18a44d6d5035d485a56124f32682d6e5dc95046396244faadf978380
 
 let package = Package(
     name: "MapboxCoreMaps",
-    platforms: [.iOS(.v10), .macOS(.v10_15), .custom("visionos", versionString: "1.0")],
+    platforms: [.iOS(.v12), .macOS(.v10_15), .custom("visionos", versionString: "1.0")],
     products: [
         .library(
             name: "MapboxCoreMaps",
             targets: ["MapboxCoreMapsWrapper"]),
     ],
     dependencies: [
-        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", from: "24.6.0"),
+        .package(url: "https://github.com/mapbox/mapbox-common-ios.git", from: "24.6.0"),
     ],
     targets: [
         .target(
             name: "MapboxCoreMapsWrapper",
-            dependencies: ["MapboxCommon", "MapboxCoreMaps"]
+            dependencies: [.product(name: "MapboxCommon", package: "mapbox-common-ios"), "MapboxCoreMaps"]
         ),
         .binaryTarget(
             name: "MapboxCoreMaps",
