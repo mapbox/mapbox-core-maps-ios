345af077c1c6cc84afec71f76b2d09e761ca44e6
Release SDK bot for GL-Native Core team <gl-native-team@mapbox.com>
2024-11-14 18:47:54 +0000

    Update SPM and CocoaPods configs for 11.9.0-sapial-release-naive-sdks-1-daily-2024-11-14-17-16 release

--- MapboxCoreMaps.podspec
+++ MapboxCoreMaps.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |m|
 
-  version = '11.9.0-daily-2024-11-07-04-25'
+  version = '11.9.0-sapial-release-naive-sdks-1-daily-2024-11-14-17-16'
 
   m.name = 'MapboxCoreMaps'
   m.version = version
--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let version = "11.9.0-daily-2024-11-07-04-25"
-let checksum = "2b46ea9a37813c00dd49250d54289302baf98833cfc0d02677b9b88d558db1e3"
+let version = "11.9.0-sapial-release-naive-sdks-1-daily-2024-11-14-17-16"
+let checksum = "1bbddcae12c24c5c07e52a53f9001b59bb36bd8871dba6267e051aa7c8443314"
 
 let package = Package(
     name: "MapboxCoreMaps",
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["MapboxCoreMapsWrapper"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/mapbox/mapbox-common-ios.git", from: "24.9.0-daily-2024-11-07-04-25"),
+        .package(url: "https://github.com/mapbox/mapbox-common-ios.git", from: "24.9.0-sapial-release-naie-sdks-1-daily-2024-11-14-17-16"),
     ],
     targets: [
         .target(
